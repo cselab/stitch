448fe0d470641ab39ae244c82b5f27b810831424
Sergey Litvinov <slitvinov@gmail.com>
2021-08-22 18:20:38 +0200

    update poc/stitch00.py

--- poc/stitch00.py
+++ poc/stitch00.py
@@ -10,7 +10,8 @@ import stitch.glb as glb
 me = "stitch0.py"
 verbose = False
 dtype = np.dtype("<u2")
-processes = multiprocessing.cpu_count()
+#processes = multiprocessing.cpu_count()
+processes = 'serial'
 sys.stderr.write("%s: processes = %s\n" % (me, processes))
 di = '/home/lisergey/stride8'
 tx, ty = 3, 5
@@ -27,6 +28,7 @@ path = (
     '01x02.raw',
     '01x01.raw',
     '01x00.raw',
+
     '00x04.raw',
     '00x03.raw',
     '00x02.raw',
@@ -50,70 +52,69 @@ for x in range(tx):
             pairs.append((i, (x + 1) * ty + y))
         if y + 1 < ty:
             pairs.append((i, x * ty + y + 1))
-src = tuple(
-    np.memmap(os.path.join(di, e), dtype, 'r', 0, (kx, ky, kz), order='F')
-    for e in path)
-glb.SRC[:] = src[:]
-alignments = tuple(stw.WobblyAlignment() for p in pairs)
+glb.SRC[:] = (np.memmap(os.path.join(di, e),
+                        dtype,
+                        'r',
+                        0, (kx, ky, kz),
+                        order='F') for e in path)
 sources = tuple(
-    glb.WobblySource(i, p, tile_position=t)
+    stw.WobblySource(i, p, tile_position=t)
     for i, (p, t) in enumerate(zip(positions, tile_positions)))
-results = st.align(pairs,
-                   sources,
-                   depth=[434 // sx, 425 // sy, None],
-                   max_shifts=[(-80 // sx, 80 // sx), (-80 // sy, 80 // sy),
-                               (-120 // sz, 120 // sz)],
-                   background=(100, 120),
-                   clip=25000,
-                   processes=processes,
-                   verbose=verbose)
+shifts, qualities = st.align(pairs,
+                             positions,
+                             tile_positions,
+                             depth=[434 // sx, 425 // sy, None],
+                             max_shifts=[(-80 // sx, 80 // sx),
+                                         (-80 // sy, 80 // sy),
+                                         (-120 // sz, 120 // sz)],
+                             background=(100, 120),
+                             clip=25000,
+                             processes=processes,
+                             verbose=verbose)
 
-
-class A:
-    pass
-
-
-aa = A()
-qualities = []
 displacements = []
-for a, (i, j), (shift, quality) in zip(alignments, pairs, results):
-    qualities.append(quality)
+for (i, j), shift, in zip(pairs, shifts):
     displacements.append(
-        tuple(q + s - p for p, q, s in zip(sources[i].position,
-                                           sources[j].position, shift)))
-    aa.displacement = tuple(
-        q + s - p
-        for p, q, s in zip(sources[i].position, sources[j].position, shift))
+        tuple(q + s - p for p, q, s in zip(positions[i], positions[j], shift)))
 
-st.place(pairs, sources, displacements)
-stw.align(pairs,
-          sources,
-          alignments,
-          max_shifts=((-20 // sx, 20 // sx), (-20 // sy, 20 // sy)),
-          prepare=True,
-          find_shifts=dict(method='tracing', cutoff=3 * np.sqrt(2)),
-          processes=processes,
-          verbose=verbose)
-stw.place(pairs,
-          alignments,
-          sources,
-          min_quality=-np.inf,
-          smooth=dict(method='window',
-                      window='hamming',
-                      window_length=100,
-                      binary=None),
-          smooth_optimized=dict(method='window',
-                                window='bartlett',
-                                window_length=20,
-                                binary=10),
-          processes=processes,
-          verbose=verbose)
+positions = st.place(pairs, sources, displacements)
+displacements, qualities, status = stw.align(
+    pairs,
+    positions,
+    max_shifts=((-20 // sx, 20 // sx), (-20 // sy, 20 // sy)),
+    prepare=True,
+    find_shifts=dict(method='tracing', cutoff=3 * np.sqrt(2)),
+    processes=processes,
+    verbose=verbose)
+positions_new, components = stw.place0(pairs,
+                                       positions,
+                                       displacements,
+                                       qualities,
+                                       status,
+                                       smooth=dict(method='window',
+                                                   window='hamming',
+                                                   window_length=100,
+                                                   binary=None),
+                                       min_quality=-np.inf,
+                                       processes=processes,
+                                       verbose=verbose)
 
-ux, uy, uz = stw.shape_wobbly(sources)
+wobble, status = stw.place1(positions,
+                            positions_new,
+                            components,
+                            sources,
+                            smooth=dict(method='window',
+                                        window='bartlett',
+                                        window_length=20,
+                                        binary=10),
+                            processes=processes,
+                            verbose=verbose)
+
+ux, uy, uz = stw.shape_wobbly(glb.SRC[0].shape, positions, wobble)
 output = "%dx%dx%dle.raw" % (ux, uy, uz)
 sink = np.memmap(output, dtype, 'w+', 0, (ux, uy, uz), order='F')
 glb.SINK[:] = [sink]
-stw.stitch(sources, processes, verbose=verbose)
+stw.stitch(sources, glb.SRC[0].shape, positions, wobble, status, processes, verbose=verbose)
 sys.stderr.write(
     "[%d %d %d] %.2g%% %s\n" %
     (*sink.shape, 100 * np.count_nonzero(sink) / np.size(sink), output))
