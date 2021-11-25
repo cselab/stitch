7443fbe1cbe30fcf1a68cf2ed0abae577f9f397b
Sergey Litvinov <slitvinov@gmail.com>
2021-11-25 23:12:32 +0100

    add read_sizes

--- stitch/mesospim.py
+++ stitch/mesospim.py
@@ -63,6 +63,54 @@ def read_meta(path):
     return D
 
 
-D = read_meta("poc/meta.txt")
-for k, v in D.items():
-    print(k, v)
+def read_sizes(pathes):
+    if not isinstance(pathes, collections.abc.Iterable):
+        raise TypeError("'%s' is not iterable" % type(pathes))
+    nx = ny = nz = psize = None
+    x_pos = []
+    y_pos = []
+    for p in pathes:
+        p = p + "_meta.txt"
+        meta = read_meta(p)
+        nx = meta["x_pixels"]
+        ny = meta["y_pixels"]
+        nz = meta["z_planes"]
+        psize = meta["Pixelsize in um"]
+        x_pos.append(meta["x_pos"])
+        y_pos.append(meta["y_pos"])
+    if nx == None:
+        raise ValueError("no 'x_pixels' meta files")
+    if ny == None:
+        raise ValueError("no 'y_pixels' meta files")
+    if nz == None:
+        raise ValueError("no 'z_planes' meta files")
+    if psize == None:
+        raise ValueError("no 'Pixelsize in um' meta files")
+    x_pos = sorted(set(x_pos))
+    y_pos = sorted(set(y_pos))
+    if len(x_pos) > 1:
+        lx = abs(x_pos[0] - x_pos[1])
+        ox = int((nx * psize - lx) / psize)
+    else:
+        ox = 0
+    if len(y_pos) > 1:
+        ly = abs(y_pos[0] - y_pos[1])
+        oy = int((ny * psize - ly) / psize)
+    else:
+        oy = 0
+    return nx, ny, nz, ox, oy
+
+
+def read_tiles(pathes):
+    pathes, tx, ty = sort_tiles(pathes)
+    nx, ny, nz, ox, oy = read_sizes(pathes)
+    return (tx, ty), (nx, ny, nz), (ox, oy), pathes
+
+
+# import glob
+# g = "/media/user/demeter16TB_3/FCD/FCD/FCD_P-OCX_2.7_NeuN-Cy3 (2)/*.raw"
+# (tx, ty), (nx, ny, nz), (ox, oy), pathes = read_tiles(glob.glob(g))
+# print(tx, ty)
+# print(nx, ny, nz)
+# print(ox, oy)
+# print(pathes)
