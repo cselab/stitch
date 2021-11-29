43fa269cb5febadfac1a1246516bbd8ab1be3991
Sergey Litvinov <slitvinov@gmail.com>
2021-11-29 16:41:56 +0100

    add initial stride

--- poc/viewer/a.py
+++ poc/viewer/a.py
@@ -13,27 +13,26 @@ except ValueError:
     path = sys.argv[1:]
 
 dtype = np.dtype("<u2")
-sx = sy = sz = 4
-ox //= sx
-oy //= sy
+stride = [8, 8, 8]
 src = [
-    np.memmap(e, dtype, 'r', 0, (nx, ny, nz), order='F')[::sx, ::sy, ::sz]
+    np.memmap(e, dtype, 'r', 0, (nx, ny, nz), order='F')
     for e in path
 ]
-kx, ky, kz = np.shape(src[0])
-
 positions = []
 for x in range(tx):
     for y in range(ty):
-        positions.append([x * (kx - ox), y * (ky - oy), 0])
+        positions.append([x * (nx - ox), y * (ny - oy), 0])
 
 fig, ax = matplotlib.pyplot.subplots()
+fig.tight_layout()
 fig.canvas.mpl_disconnect(fig.canvas.manager.key_press_handler_id)
 ax.axis('off')
-ax.set_xlim((0, kx * tx))
-ax.set_ylim((0, ky * ty))
+lx = nx * tx
+ly = ny * ty
+ax.set_xlim((-lx/10, 11*lx/10))
+ax.set_ylim((-ly/10, 11*ly/10))
 
-zslice = [kz // 2]
+zslice = [nz // 2]
 se = [0]
 art = [None for e in src]
 
@@ -42,11 +41,11 @@ def draw(i):
     z += zslice[0]
     if z < 0:
         z = 0
-    elif z > kz - 1:
-        z = kz - 1
-    s = src[i][:, :, z]
-    y = ty * ky - y
-    extent = (x, x + kx, y - ky, y)
+    elif z > nz - 1:
+        z = nz - 1
+    s = src[i][::stride[0], ::stride[1], z]
+    y = ty * ny - y
+    extent = (x, x + nx, y - ny, y)
     cmap = 'Greens' if se[0] == i else 'Greys'
     if art[i] is not None:
         art[i].remove()
@@ -57,53 +56,52 @@ def draw(i):
 def press(event):
     n = len(src)
     key = event.key
-    print(key)
-    if key == "right":
-        zslice[0] += 1
-        for i in range(n):
-            draw(i)
-        fig.canvas.draw()
-    elif key == "left":
-        zslice[0] -= 1
-        for i in range(n):
-            draw(i)
-        fig.canvas.draw()
-    elif key == "up":
+    if key == "down":
         se[0] += 1
         if se[0] == n:
             se[0] = 0
         for i in range(n):
             draw(i)
         fig.canvas.draw()
-    elif key == "down":
+    elif key == "up":
         se[0] -= 1
         if se[0] == -1:
             se[0] = n - 1
         for i in range(n):
             draw(i)
         fig.canvas.draw()
+    elif key == "right":
+        zslice[0] += stride[2]
+        for i in range(n):
+            draw(i)
+        fig.canvas.draw()
+    elif key == "left":
+        zslice[0] -= stride[2]
+        for i in range(n):
+            draw(i)
+        fig.canvas.draw()
     elif key == "h":
-        positions[se[0]][0] -= 1
+        positions[se[0]][0] -= stride[0]
         draw(se[0])
         fig.canvas.draw()
     elif key == "l":
-        positions[se[0]][0] += 1
+        positions[se[0]][0] += stride[0]
         draw(se[0])
         fig.canvas.draw()
     elif key == "j":
-        positions[se[0]][1] -= 1
+        positions[se[0]][1] -= stride[1]
         draw(se[0])
         fig.canvas.draw()
     elif key == "k":
-        positions[se[0]][1] += 1
+        positions[se[0]][1] += stride[1]
         draw(se[0])
         fig.canvas.draw()
     elif key == "i":
-        positions[se[0]][2] -= 1
+        positions[se[0]][2] -= stride[2]
         draw(se[0])
         fig.canvas.draw()
     elif key == "n":
-        positions[se[0]][2] += 1
+        positions[se[0]][2] += stride[2]
         draw(se[0])
         fig.canvas.draw()
     elif key == "z":
@@ -114,8 +112,27 @@ def press(event):
             art[se[0]].remove()
             art[se[0]] = None
             fig.canvas.draw()
+    elif key == "R":
+        stride[0] += 1
+        stride[1] += 1
+        stride[2] += 1
+        for i in range(n):
+            draw(i)
+        fig.canvas.draw()
+    elif key == "r":
+        stride[0] -= 1
+        stride[1] -= 1
+        stride[2] -= 1
+        stride[:] = [max(1, e) for e in stride]
+        for i in range(n):
+            draw(i)
+        fig.canvas.draw()
     elif key == "q":
         sys.exit(0)
+    elif key == "s":
+        print()
+        for x, y, z in positions:
+            print(x, y, z)
 
 for i in range(len(src)):
     draw(i)
